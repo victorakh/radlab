2d487d95c73fe06bdb11d19d96f671259ee2fd25
victorakh <133075156+victorakh@users.noreply.github.com>
2023-07-18 10:01:56 +0800

    Update variables.tf

--- modules/data_science_L/variables.tf
+++ modules/data_science_L/variables.tf
@@ -163,7 +163,7 @@ variable "folder_id" {
 variable "gpu_accelerator_type" {
   description = "Type of GPU you would like to spin up. {{UIMeta group=3 order=10 }}"
   type        = string
-  default     = "NVIDIA_TESLA_L4"
+  default     = "NVIDIA_L4"
 }
 
 variable "gpu_accelerator_core_count" {
