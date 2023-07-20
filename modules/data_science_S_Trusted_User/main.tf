d5b832bf70076cdb36eac4e54bf1376eb784baec
victorakh <133075156+victorakh@users.noreply.github.com>
2023-07-20 22:44:06 +0800

    Update main.tf

--- modules/data_science_S_Trusted_User/main.tf
+++ modules/data_science_S_Trusted_User/main.tf
@@ -175,8 +175,10 @@ resource "google_service_account_iam_member" "sa_ai_notebook_iam" {
 resource "null_resource" "ai_notebook_usermanaged_provisioning_state" {
   for_each = toset(google_notebooks_instance.ai_notebook_usermanaged[*].name)
   provisioner "local-exec" {
-    command = "while [ \"$(gcloud notebooks instances list --location ${var.zone} --project ${local.project.project_id} --filter 'NAME:${each.value} AND STATE:ACTIVE' --format 'value(STATE)' | wc -l | xargs)\" != 1 ]; do echo \"${each.value} not active yet.\"; done"
-  }
+    #command = "while [ \"$(gcloud notebooks instances list --location ${var.zone} --project ${local.project.project_id} --filter 'NAME:${each.value} AND STATE:ACTIVE' --format 'value(STATE)' | wc -l | xargs)\" != 1 ]; do echo \"${each.value} not active yet.\"; done"
+    #hardcode Singapore to bypass the region/zone bug
+    command = "while [ \"$(gcloud notebooks instances list --location \"asia-southeast1-a\" --project ${local.project.project_id} --filter 'NAME:${each.value} AND STATE:ACTIVE' --format 'value(STATE)' | wc -l | xargs)\" != 1 ]; do echo \"${each.value} not active yet.\"; done"  
+}
 
   depends_on = [google_notebooks_instance.ai_notebook_usermanaged]
 }
@@ -185,7 +187,9 @@ resource "google_notebooks_instance" "ai_notebook_usermanaged" {
   count        = var.notebook_count > 0 && var.create_usermanaged_notebook ? var.notebook_count : 0
   project      = local.project.project_id
   name         = "usermanaged-notebooks-${count.index + 1}"
-  location     = var.zone
+  #hardcode Singapore to bypass the region/zone bug
+  #location     = var.zone
+  location = "asia-southeast1-a"
   machine_type = var.machine_type
 
   dynamic "vm_image" {
