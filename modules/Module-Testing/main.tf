a38f07ebbdb5e388bf895c44d6aa72f0c7d57016
victorakh <133075156+victorakh@users.noreply.github.com>
2023-09-08 11:01:36 +0800

    Update main.tf

--- modules/Module-Testing/main.tf
+++ modules/Module-Testing/main.tf
@@ -158,22 +158,25 @@ resource "google_service_account" "sa_p_notebook" {
   display_name = "Notebooks in trusted environment"
 }
 
-/**
+
 resource "google_project_iam_member" "sa_p_notebook_permissions" {
   for_each = toset(local.notebook_sa_project_roles)
   project  = local.project.project_id
   member   = "serviceAccount:${google_service_account.sa_p_notebook.email}"
   role     = each.value
 }
-*/
 
 
+/**  To disable STE CDCS Users notebook access
 resource "google_service_account_iam_member" "sa_ai_notebook_iam" {
   for_each           = toset(concat(formatlist("user:%s", var.trusted_users), formatlist("group:%s", var.trusted_groups)))
   member             = each.value
   role               = "roles/iam.serviceAccountUser"
   service_account_id = google_service_account.sa_p_notebook.id
 }
+
+*/
+
 
 resource "null_resource" "ai_notebook_usermanaged_provisioning_state" {
   for_each = toset(google_notebooks_instance.ai_notebook_usermanaged[*].name)
